de4ff77a81808f32e949a094d6545b265a8dcfff
Ernst de Vries <edevries@victronenergy.com>
2016-07-22 11:55:48 +0200

    test now also building test programs

--- build.sh
+++ build.sh
@@ -2,42 +2,11 @@
 
 ROOT=$PWD
 
-rm -rf build
-
-qtchooser -l
-
 export QT_SELECT=4
 
 mkdir -p build/dbus-cgwacs
 cd build/dbus-cgwacs
 qmake CXX=$CXX ../../software/dbus-cgwacs.pro && make
-if [[ $? -ne 0 ]] ; then
-    exit 1
-fi
-
-cd $ROOT
-
-mkdir -p build/maintenance_lab
-cd build/maintenance_lab
-qmake CXX=$CXX ../../test/maintenance_lab/maintenance_lab.pro && make
-if [[ $? -ne 0 ]] ; then
-    exit 1
-fi
-
-cd $ROOT
-
-mkdir -p build/control_loop_test
-cd build/control_loop_test
-qmake CXX=$CXX ../../test/control_loop_test/control_loop_test.pro && make
-if [[ $? -ne 0 ]] ; then
-    exit 1
-fi
-
-cd $ROOT
-
-mkdir -p build/control_loop_lab
-cd build/control_loop_lab
-qmake CXX=$CXX ../../test/control_loop_lab/control_loop_lab.pro && make
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
--- test.sh
+++ test.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-./build/control_loop_lab/control_loop_lab
+export QT_SELECT=qt4
+
+ROOT=$PWD
+
+mkdir -p build/control_loop_lab
+cd build/control_loop_lab
+qmake CXX=$CXX ../../test/control_loop_lab/control_loop_lab.pro && make && ./control_loop_lab
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
 
-./build/maintenance_lab/maintenance_lab
+cd $ROOT
+
+mkdir -p build/maintenance_lab
+cd build/maintenance_lab
+qmake CXX=$CXX ../../test/maintenance_lab/maintenance_lab.pro && make && ./maintenance_lab
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
 
-./build/control_loop_test/control_loop_test
+cd $ROOT
+
+mkdir -p build/control_loop_test
+cd build/control_loop_test
+qmake CXX=$CXX ../../test/control_loop_test/control_loop_test.pro && make && ./control_loop_test
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
